a73438aa008f5223a847009151b806aad2fce88a
msteele96 <marcsteele2014@gmail.com>
2021-06-24 20:21:00 -0400

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      text: ""
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    // const action = {
+    //   type: 'ADD_RESTAURANT',
+    //   restuarant: event.target.value
+    // }
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ""
+    })
+    // this.props.addRestaurant
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <label>Restaurant Name</label>
+          <input type="text" value={this.state.text} onChange={this.handleChange}></input>
+          <input type="submit"></input>
+        </form>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,18 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = [], action) {
+    
+    switch (action.type) {
+        case 'ADD_RESTAURANT':
+            const restaurant = {
+                text: action.text,
+                id: cuid()
+            }
+            return state.concat(restaurant)
 
+        case '':
+            return
+        default:
+            return state
+    }
 }
