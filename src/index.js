b9304057f6ed3c149191e0e04c5bf6c3ae753d0f
msteele96 <marcsteele2014@gmail.com>
2021-06-25 18:19:40 -0400

    commit tree 9eb274e2e2163b5f55000e0eceba81c371da65a5

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import manageRestaurant from './reducers/manageRestaurant';
-import rootReducer from './reducers/index'
-
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import manageRestaurants from './reducers/manageRestaurant';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(manageRestaurants, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 ReactDOM.render(
